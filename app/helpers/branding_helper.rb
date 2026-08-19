a583fc828367e7c45c67a15bbff9d9c5ec1bbc92
juroom2005 <juroom9841@gmail..com>
2026-08-19 16:37:49 +0900

    flashmob 스킨 커스텀: 포인트 컬러(라이트 파랑/다크 노랑), 배경 이미지, 로고 교체

--- app/helpers/branding_helper.rb
+++ app/helpers/branding_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 module BrandingHelper
+  # flashmob 커스텀:
+  #   원본은 워드마크/아이콘을 <svg><use href="#symbol"> 로 그렸는데, 우리 로고
+  #   SVG 는 내부에 gradient/pattern(url(#...)) 을 쓰기 때문에 <use> 로는 그
+  #   참조가 깨져 안 그려진다. → 완결된 SVG 를 image_tag 로 직접 넣어 해결.
+  #   (React logo.tsx 를 <img> 로 바꾼 것과 동일한 처리)
+  #
+  #   · 워드마크(:wordmark) = logo-full.svg (flashmob 가로 로고)
+  #   · 아이콘(:icon)       = logo.svg      (정사각 심볼, 파랑 그라데)
+
   def logo_as_symbol(version = :icon)
     case version
     when :icon
@@ -11,17 +20,28 @@ module BrandingHelper
   end
 
   def _logo_as_symbol_wordmark
-    tag.svg(viewBox: '0 0 261 66', class: 'logo logo--wordmark') do
-      tag.title('Mastodon') +
-        tag.use(href: '#logo-symbol-wordmark')
-    end
+    image_tag(
+      frontend_asset_path('images/logo-full.svg'),
+      alt: 'Mastodon',
+      class: 'logo logo--wordmark'
+    )
   end
 
   def _logo_as_symbol_icon
-    tag.svg(tag.use(href: '#logo-symbol-icon'), viewBox: '0 0 79 79', class: 'logo logo--icon')
+    image_tag(
+      frontend_asset_path('images/logo.svg'),
+      alt: 'Mastodon',
+      class: 'logo logo--icon'
+    )
   end
 
+  # 설정·관리자·리다이렉트 등 페이지 좌상단 로고.
+  # SUMMER Flash Mob 풀로고(PNG)로 표시.
   def render_logo
-    image_tag(frontend_asset_path('images/logo.svg'), alt: 'Mastodon', class: 'logo logo--icon')
+    image_tag(
+      frontend_asset_path('images/logo-flashmob-full.png'),
+      alt: 'Mastodon',
+      class: 'logo logo--full'
+    )
   end
 end
--- app/javascript/mastodon/components/logo.tsx
+++ app/javascript/mastodon/components/logo.tsx
@@ -1,23 +1,25 @@
 import classNames from 'classnames';
 
 import logo from '@/images/logo.svg';
+import logoFull from '@/images/logo-full.svg';
+
+// flashmob 커스텀:
+//   기존 WordmarkLogo/IconLogo 는 <use xlink:href="#symbol"> 로 심볼을 참조했는데,
+//   우리 로고 SVG 는 내부에 gradient/pattern(url(#...)) 를 쓰기 때문에 <use> 로
+//   복제될 때 그 참조가 shadow tree 경계를 못 넘어 안 그려졌다.
+//   → 완결된 SVG 파일을 <img> 로 직접 넣어 문제를 원천 제거.
+//   (SymbolLogo 가 원래 쓰던 <img> 방식과 동일. gradient/pattern 정상 렌더)
 
 export const WordmarkLogo: React.FC = () => (
-  <svg viewBox='0 0 261 66' className='logo logo--wordmark' role='img'>
-    <title>Mastodon</title>
-    <use xlinkHref='#logo-symbol-wordmark' />
-  </svg>
+  <img src={logoFull} alt='Mastodon' className='logo logo--wordmark' />
 );
 
 export const IconLogo: React.FC<{ className?: string }> = ({ className }) => (
-  <svg
-    viewBox='0 0 79 79'
+  <img
+    src={logo}
+    alt='Mastodon'
     className={classNames('logo logo--icon', className)}
-    role='img'
-  >
-    <title>Mastodon</title>
-    <use xlinkHref='#logo-symbol-icon' />
-  </svg>
+  />
 );
 
 export const SymbolLogo: React.FC = () => (
